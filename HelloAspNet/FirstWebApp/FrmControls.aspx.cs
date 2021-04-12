5f3fcc5a9777a3637641067d34ece09c43ac8414
vustkdgus <vustkdgus@naver.com>
2021-04-13 08:51:38 +0900

    21.04.12 커밋

---
+++ HelloAspNet/FirstWebApp/FrmControls.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstWebApp
+{
+    public partial class FrmControls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LsbHobby.Items.Add("농구");
+            LsbHobby.Items.Add("배구");
+
+            CboPhoneNum.Items.Add("010-2222-1234");
+
+            if (!Page.IsPostBack)
+            {
+                CtlHidden.Value = DateTime.Now.ToString();
+            }
+        }
+
+        protected void BtnChange_Click(object sender, EventArgs e)
+        {
+            ImgChange.ImageUrl = "~/images/bbb.png";
+        }
+
+        protected void BtnOK_Click(object sender, EventArgs e)
+        {
+            Response.Write(CtlHidden.Value);
+        }
+    }
+}
---
+++ HelloAspNet/aaa/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aaa.Global" Language="C#" %>
