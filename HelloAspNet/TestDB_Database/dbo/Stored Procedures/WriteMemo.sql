058c68c890f0f0f1b6b6db1faad5b3ed34f90ea2
vustkdgus <vustkdgus@naver.com>
2021-04-14 12:36:12 +0900

    21.04.14 중간커밋

---
+++ HelloAspNet/TestDB_Database/dbo/Stored Procedures/WriteMemo.sql
@@ -0,0 +1,12 @@
+﻿
+CREATE Procedure [dbo].[WriteMemo]
+(
+	@Name NVarChar(25),
+	@Email NVarChar(100),
+	@Title NVarChar(150),
+	@PostDate Datetime,
+	@PostIP NVarChar(15)
+)
+As
+	Insert Memos(Name, Email, Title, PostDate, PostIP)
+	Values(@Name, @Email, @Title, @PostDate, @PostIP)
---
+++ HelloAspNet/TestDB_Database/dbo/Tables/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[UID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] NVARCHAR(25) NOT NULL, 
+    [Password] VARCHAR(200) NOT NULL
+)
