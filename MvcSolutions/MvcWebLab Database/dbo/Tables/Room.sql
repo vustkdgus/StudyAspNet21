b3d937eb1cdbde3abb79f8725d4a4803ee4eb1e2
vustkdgus <vustkdgus@naver.com>
2021-04-19 17:04:09 +0900

    21.04.19 커밋

---
+++ MvcSolutions/MvcWebLab Database/dbo/Tables/Room.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Room] (
+    [RoomId] INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (20) NOT NULL,
+    CONSTRAINT [PK_Room] PRIMARY KEY CLUSTERED ([RoomId] ASC)
+);
+
---
+++ MvcSolutions/MvcWebLab Database/dbo/Tables/Speakers.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Speakers] (
+    [SpeakerId] INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (25) NOT NULL,
+    [RoomId]    INT           NOT NULL,
+    CONSTRAINT [PK_Speakers] PRIMARY KEY CLUSTERED ([SpeakerId] ASC),
+    CONSTRAINT [FK_Speakers_Room_RoomId] FOREIGN KEY ([RoomId]) REFERENCES [dbo].[Room] ([RoomId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Speakers_RoomId]
+    ON [dbo].[Speakers]([RoomId] ASC);
+
